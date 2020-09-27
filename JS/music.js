a89e1c74bb2f18883eda1b467b3eae84e942921e
joudi awameh <awamehj@gmail.com>
2020-09-27 22:08:58 +0300

    added javascript

---
+++ JS/music.js
@@ -0,0 +1,50 @@
+'use strict';
+var rockItem = [];
+function Rock(name) {
+  this.name = name;
+  rockItem.push(this);
+}
+if(localStorage.getItem('rock')){
+  rockItem = JSON.parse(localStorage.getItem('rock'));
+}else{
+  new Rock('Remember Everything');
+  new Rock('Wrong Side of Heaven');
+  new Rock('Everybody Loves An Outlaw ');
+  new Rock('Black');
+  new Rock('Jeremy');
+}
+
+var heart = document.getElementById('heart');
+var heart1 = document.getElementById('heart1');
+var heart2 = document.getElementById('heart2');
+var heart3 = document.getElementById('heart3');
+var heart4 = document.getElementById('heart4');
+
+
+heart.addEventListener('click', addfavourite);
+heart1.addEventListener('click', addfavourite);
+heart2.addEventListener('click', addfavourite);
+heart3.addEventListener('click', addfavourite);
+heart4.addEventListener('click', addfavourite);
+
+function addfavourite(event) {
+
+  if (event.target.id === 'heart' ) {
+
+    localStorage.setItem('rock', JSON.stringify(rockItem[0]));
+
+  }else if (event.target.id === 'heart1' ){
+    localStorage.setItem('rock', JSON.stringify(rockItem[1]));
+  }else if (event.target.id === 'heart2' ){
+    localStorage.setItem('rock', JSON.stringify(rockItem[2]));
+  }else if (event.target.id === 'heart3' ){
+    localStorage.setItem('rock', JSON.stringify(rockItem[3]));
+  }else if (event.target.id === 'heart4' ){
+    localStorage.setItem('rock', JSON.stringify(rockItem[4]));
+  }
+
+  
+}
+
+
+
