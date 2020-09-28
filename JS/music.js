68661e4a45b48ac285c61b2d9684e9dd81ed3c9c
batoolalomari <batoolalomari2@gmail.com>
2020-09-28 13:06:29 +0300

    add to favorite

--- JS/music.js
+++ JS/music.js
@@ -1,18 +1,32 @@
 'use strict';
+
+
+//= [];
+
 var rockItem= [];
 function Rock(name) {
   this.name = name;
   rockItem.push(this);
 }
+
 if(localStorage.getItem('rock')){
   rockItem = JSON.parse(localStorage.getItem('rock'));
-}else{
-  new Rock('Remember Everything');
-  new Rock('Wrong Side of Heaven');
-  new Rock('Everybody Loves An Outlaw ');
-  new Rock('Black');
-  new Rock('Jeremy');
+}else { 
+new Rock('Remember Everything');
+new Rock('Wrong Side of Heaven');
+new Rock('Everybody Loves An Outlaw');
+new Rock('Black');
+new Rock('Jeremy');
 }
+
+if(localStorage.getItem('rock')){
+  var rockClick = JSON.parse(localStorage.getItem('rock'));
+}else { 
+  var rockClick=[];
+}
+
+
+
 
 var heart = document.getElementById('heart');
 var heart1 = document.getElementById('heart1');
@@ -21,30 +35,54 @@ var heart3 = document.getElementById('heart3');
 var heart4 = document.getElementById('heart4');
 
 
-heart.addEventListener('lclick', addfavourite);
+heart.addEventListener('click', addfavourite);
 heart1.addEventListener('click', addfavourite);
 heart2.addEventListener('click', addfavourite);
 heart3.addEventListener('click', addfavourite);
 heart4.addEventListener('click', addfavourite);
 
+/*heart.addEventListener('change', removefavourite);
+heart1.addEventListener('change', removefavourite);
+heart2.addEventListener('change', removefavourite);
+heart3.addEventListener('change', removefavourite);
+heart4.addEventListener('change', removefavourite);*/
+
 function addfavourite(event) {
 
-  if (event.target.id === 'heart' ) {
+  /*if (localStorage.getItem('rock')) {
+     rockItem = JSON.parse(localStorage.getItem('rock'));
+  
+  }
+  else {*/
+  if (event.target.id === 'heart') {
+ 
+    rockClick.push('Remember Everything');
+    localStorage.setItem('rock', JSON.stringify(rockClick));
+    
 
-    localStorage.setItem('rock', JSON.stringify(rockItem[0]));
+    /*clickItem.push(rockItem[0]);
+    localStorage.setItem('rock', JSON.stringify(clickItem[0]));*/
+         
+  } else if (event.target.id === 'heart1') {
+    rockClick.push('Wrong Side of Heaven');
+    localStorage.setItem('rock', JSON.stringify(rockClick));
+    //clickItem.push(rockItem[1]);
 
-  }else if (event.target.id === 'heart1' ){
-    localStorage.setItem('rock', JSON.stringify(rockItem[1]));
-  }else if (event.target.id === 'heart2' ){
-    localStorage.setItem('rock', JSON.stringify(rockItem[2]));
-  }else if (event.target.id === 'heart3' ){
-    localStorage.setItem('rock', JSON.stringify(rockItem[3]));
-  }else if (event.target.id === 'heart4' ){
-    localStorage.setItem('rock', JSON.stringify(rockItem[4]));
+  } else if (event.target.id === 'heart2') {
+    rockClick.push('Everybody Loves An Outlaw');
+    localStorage.setItem('rock', JSON.stringify(rockClick));
+   // clickItem.push(rockItem[2]);
+
+  } else if (event.target.id === 'heart3') {
+    rockClick.push('Black');
+    localStorage.setItem('rock', JSON.stringify(rockClick));
+    //clickItem.push(rockItem[3]);
+
+  } else if (event.target.id === 'heart4') {
+    rockClick.push('Jeremy');
+    localStorage.setItem('rock', JSON.stringify(rockClick));
+    //clickItem.push(rockItem[4]);
   }
 
-  
+
 }
-
-
-
