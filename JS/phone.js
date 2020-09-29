4b549dcf4953fc24de44e216ca6ab083336e8976
Omar Zain <omaren2010@gmail.com>
2020-09-29 15:51:17 +0300

    added phone app

---
+++ JS/phone.js
@@ -0,0 +1,38 @@
+'use strict';
+
+// touch sound
+var s1 = new Audio();
+s1.src = '../images/phone/num1.mp3';
+
+var s2 = new Audio();
+s2.src = '../images/phone/num2.mp3';
+
+var s3 = new Audio();
+s3.src = '../images/phone/num3.mp3';
+
+var s4 = new Audio();
+s4.src = '../images/phone/num4.mp3';
+
+var s5 = new Audio();
+s5.src = '../images/phone/num5.mp3';
+
+var s6 = new Audio();
+s6.src = '../images/phone/num6.mp3';
+
+var s7 = new Audio();
+s7.src = '../images/phone/num7.mp3';
+
+var s8 = new Audio();
+s8.src = '../images/phone/num8.mp3';
+
+var s9 = new Audio();
+s9.src = '../images/phone/num9.mp3';
+
+var s0 = new Audio();
+s0.src = '../images/phone/num0.mp3';
+
+var star = new Audio();
+star.src = '../images/phone/star.mp3';
+
+var hash = new Audio();
+hash.src = '../images/phone/hash.mp3';
